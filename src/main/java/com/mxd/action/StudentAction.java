1d9c0a6826423f96a521c6bdc00fc8c81a1f86d0
mxd2018 <1406988305@qq.com>
2018-03-08 20:53:54 +0800

    修改pom web.xml db.properti文件，将war包部署到阿里云远程服务器

--- src/main/java/com/mxd/action/StudentAction.java
+++ src/main/java/com/mxd/action/StudentAction.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -26,7 +25,7 @@ public class StudentAction {
 	@RequestMapping("/addStudent")
 	public String addStudent(Student student){
 		service.addStudent(student);
-		return "index";
+		return "index2";
 	}
 	
 	@RequestMapping("/listAllStudent/{currentPage}")
